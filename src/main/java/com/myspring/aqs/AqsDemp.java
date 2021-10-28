4a0404ef18d32a754b5fcfa9f3d040df1cbaf27a
yyymmp <j2538474647@163.com>
2021-10-28 23:59:47 +0800

    AQS源码分析

--- src/main/java/com/myspring/aqs/AqsDemp.java
+++ src/main/java/com/myspring/aqs/AqsDemp.java
@@ -1,5 +1,6 @@
 package com.myspring.aqs;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -11,6 +12,38 @@ import java.util.concurrent.locks.ReentrantLock;
 public class AqsDemp {
 
     public static void main(String[] args) {
-        ReentrantLock reentrantLock = new ReentrantLock();
+        ReentrantLock lock = new ReentrantLock();
+        //模拟三个线程办理业务
+
+        //a是第一个顾客 没有任何人 直接去办
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("a come");
+                TimeUnit.SECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "a").start();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("b come");
+            } finally {
+                lock.unlock();
+            }
+        }, "b").start();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("c come");
+            } finally {
+                lock.unlock();
+            }
+        }, "c").start();
     }
 }
